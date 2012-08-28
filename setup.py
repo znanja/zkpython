b9a3665a95c21b0f03d680a0b6a27915f0835f1f
Daniel Enman <enmand@unerror.com>
2012-08-27 21:46:23 -0300

    Update setup.py
    lib for zookeeper_mt

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ zookeepermodule = Extension("zookeeper",
                             include_dirs=["/usr/include/c-client-src", "/usr/local/include/c-client-src",
                                     "/usr/include/zookeeper", "/usr/local/include/zookeeper",
                                     "/opt/local/include/zookeeper"],
-                            libraries=["libzookeeper_mt"],
+                            libraries=["zookeeper_mt"],
                             )
 
 setup( name="zkpython",
